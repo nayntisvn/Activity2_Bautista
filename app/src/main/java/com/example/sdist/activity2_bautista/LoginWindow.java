2e9c4ae5b60cf18296efc1a1788b4a20b97f2935
--null <=>
2017-07-18 14:28:48 +0800

    Initial commit

---
+++ app/src/main/java/com/example/sdist/activity2_bautista/LoginWindow.java
@@ -0,0 +1,13 @@
+package com.example.sdist.activity2_bautista;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginWindow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_window);
+    }
+}
